00052125ba60e13b4236af62033227a7f4d7ce26
jGohan-cpu <jjoseriveram01@gmail.com>
2023-08-22 21:47:32 -0400

    0 and 2 3 fixed json fixed

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -13,24 +13,19 @@ if __name__ == "__main__":
     users_todo_dict = {}
 
     for user in users:
-        # Extracting actual user id for extracts its TODO tasks
         user_id = user.get('id')
         user_todo_query = {'userId': user_id}
         response_2 = requests.get("https://jsonplaceholder.typicode.com/todos",
                                   params=user_todo_query)
         todo_list = response_2.json()
 
-        # Creating a list of user's tasks (each contained in dictionary form)
         username = user.get('username')
         tasks = [{"task": task.get('title'), "username": username,
                   "completed": task.get('completed')} for task in todo_list]
 
-        # Updating users task's dictionary with actual user and its tasks
         users_todo_dict[user_id] = tasks
 
-    # Serializing to json
     json_object = json.dumps(users_todo_dict)
 
-    # Writing to json file
     with open('todo_all_employees.json', "w") as jsonfile:
         jsonfile.write(json_object)
